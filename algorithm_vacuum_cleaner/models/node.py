36d98c42c8963793d4343c5d3343c9571b141d25
1-a-2-b-3-c <pnqhoang06@gmail.com>
2026-05-29 22:02:32 +0700

    update

--- algorithm_vacuum_cleaner/models/node.py
+++ algorithm_vacuum_cleaner/models/node.py
@@ -5,9 +5,8 @@ class Node:
         self.parent = parent
         self.direction = direction
         self.cost = cost if cost is not None else 0
-
         if state is None:
-            self.state, self.x, self.y = self.create()
+            self.state, self.x, self.y = self.create() 
         else:
             self.state = state
             self.x = x
@@ -17,22 +16,18 @@ class Node:
         while True:
             m = random.randint(4, 6)
             n = random.randint(4, 6)
-
             state = [[random.choice([-1, 0, 1]) for _ in range(n)] for _ in range(m)]
 
             has_dirt = any(state[i][j] == 1 for i in range(m) for j in range(n))
-
             if not has_dirt:
                 continue
-
+            
             return state, None, None
 
     def goal(self):
         g = copy.deepcopy(self.state)
-
         for i in range(len(g)):
             for j in range(len(g[0])):
                 if self.state[i][j] == 1:
                     g[i][j] = 0
-
         return g
